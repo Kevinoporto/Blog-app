8be1b20d70b2b124fb704aa5bb769a9d4673da3a
Kevinoporto <kevinoporto.h@gmail.com>
2025-01-17 22:19:29 -0400

    Add a component

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,16 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Blog App</title>
+	<script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-	<h1>Home Page</h1>
+	<div>
+		<x-alert type="success">
+			<x-slot name='title'>
+				Hablame manito 
+			</x-slot>
+			Qloq!
+		</x-alert>
+	</div>
 </body>
 </html>
