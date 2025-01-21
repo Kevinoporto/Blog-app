f0e91e0b991c4ff7486f71a4610134b6c39012af
Kevinoporto <kevinoporto.h@gmail.com>
2025-01-20 23:11:50 -0400

    Add a factorie

--- database/migrations/2025_01_19_171147_create_posts_table.php
+++ database/migrations/2025_01_19_171147_create_posts_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
 			$table->string('title');
 			$table->longText('content');
-			$table->string('categorie');
+			$table->string('category');
             $table->timestamps();
         });
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $user = new User();
+		
+		$user -> name = 'kevin admin';
+		$user -> email = 'kevinadmin@gmail.com';
+		$user -> password = bcrypt('123456');
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+		$user -> save();
+
+		Post::factory(10)->create();
     }
 }
